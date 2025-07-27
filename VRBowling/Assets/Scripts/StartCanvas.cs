3f02b20e7493cbd31ba6dba803a179c7e1a8b449
yadan1 <17768282416@163.com>
2025-07-27 20:25:34 +0800

    主界面

---
+++ VRBowling/Assets/Scripts/StartCanvas.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class StartCanvas : MonoBehaviour
+{
+    public Transform m_BtnStart;//开始按钮
+    public Transform m_BtnEnd;//结束按钮
+    void Start()
+    {
+        
+    }
+    #region 开始按钮点击、进入、退出
+    public void OnStartClick()
+    {
+        
+    }
+    public void OnStart_EnterClick()
+    {
+        m_BtnStart.DOScale(Vector3.one * 1.1f, 0.1f);
+    }
+    public void OnStart_ExitClick()
+    {
+        m_BtnStart.DOScale(Vector3.one * 1f, 0.1f);
+    }
+
+    #endregion
+
+    #region 退出按钮点击、进入、退出
+    public void OnEndClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void OnEnd_EnterClick()
+    {
+        m_BtnEnd.DOScale(Vector3.one * 1.1f, 0.1f);
+    }
+    public void OnEnd_ExitClick()
+    {
+        m_BtnEnd.DOScale(Vector3.one * 1f, 0.1f);
+    }
+
+    #endregion
+}
